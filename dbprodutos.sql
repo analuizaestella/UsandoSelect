80f8a862ee66a96808435ffa6efbd432d5b86aba
analuizaestella <analuizafranciscoestella@gmail.com>
2024-08-23 20:57:10 -0300

    Comandos de BD2

--- dbprodutos.sql
+++ dbprodutos.sql
@@ -77,6 +77,8 @@ select valor * 1.20 as 'Valor' from produtos where nome like 'f%';
 update produtos set valor = valor * 1.12 where nome like 'f%';
 select nome, valor from produtos where nome like 'f%';
 
+update produtos set quantidade = quantidade + 50 where valor > 400 and valor < 600;
+select * from produtos where valor > 400 and valor < 600; 
 
 
 
